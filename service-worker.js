463d4c10b2b15a776192547c343c60024cf59232
HeetdevLab <155948193+HeetDevLab@users.noreply.github.com>
2026-02-26 16:40:48 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,17 @@ const OFFLINE_URL = "/Webtools/offline.html";
 
 self.addEventListener("install", event => {
   self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.add(OFFLINE_URL);
+      return cache.addAll([
+        "/Webtools/offline.html",
+        "/Webtools/index.html",
+        "/Webtools/tools/password-checker.html",
+        "/Webtools/tools/pin-generator.html",
+        "/Webtools/tools/password-generator.html",
+        "/Webtools/tools/jwt-decoder.html"
+      ]);
     })
   );
 });
