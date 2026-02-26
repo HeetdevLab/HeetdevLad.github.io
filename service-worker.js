89370b15a3dbaf76e2ad894c735bf23748072588
HeetdevLab <155948193+HeetDevLab@users.noreply.github.com>
2026-02-26 16:07:34 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "heetdevlab-global-v1";
+const OFFLINE_URL = "/Webtools/offline.html";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.add(OFFLINE_URL);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(OFFLINE_URL);
+    })
+  );
+});
